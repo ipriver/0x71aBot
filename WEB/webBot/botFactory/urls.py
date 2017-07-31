6012ee71d63922aa948105ba0bee9b591564cbc9
ipriver <maxdrozdov18@gmail.com>
2017-07-31 21:39:52 +0300

    authentication

--- WEB/webBot/botFactory/urls.py
+++ WEB/webBot/botFactory/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'botFactory'
 urlpatterns = [
-    url(r'^$', views.test, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^sign_in/$', views.sign_in, name='sign_in'),
+    url(r'^logout/$', views.user_logout, name='sign_in'),
 ]
--- WEB/webBot/botFactory/views.py
+++ WEB/webBot/botFactory/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from .forms import SignInForm, RegForm
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
-def test(request):
+def index(request):
     template_name = 'botFactory/index.html'
     context = {
-        'message': '',
         'authenticated': False,
         'form': None,
     }
     if request.user.is_authenticated:
-        context['message'] = 'Hello, user'
+        context['message'] = 'Hello, ' + request.user.username
         context['authenticated'] = True
     else:
-        context['message'] = 'Анонимный пользователь'
         context['form'] = SignInForm()
     return render(request, template_name, context)
 
@@ -24,3 +26,26 @@ def register(request):
         'form': RegForm()
     }
     return render(request, template_name, context)
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['login']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+            return HttpResponseRedirect('/')
+    else:
+        form = SignInForm()
+        context = {
+            'authenticated': False,
+            'form': form,
+        }
+    return render(request, template_name, context)
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
