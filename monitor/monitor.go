41024ea2ac397e2960db65c450446b723365f737
ipriver <maxdrozdov18@gmail.com>
2017-09-06 22:51:22 +0300

    removed ping-pong

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -13,22 +13,9 @@ func MonitorChannel(conn net.Conn, conf *config.Config) {
 		rb, _ := conn.Read(buff)
 		bStri := string(buff[:rb])
 		fmt.Println(bStri)
-		err := AnswerToTwitch(strings.Split(bStri, " "), conn)
-		if err != nil {
-			fmt.Println("PING-PONG answer error occured")
-		}
 		d := strings.Split(bStri, ":")
 		mes := d[len(d)-1]
 		LookForCommands(mes, conn)
 	}
 
-}
-
-func AnswerToTwitch(mesString []string, conn net.Conn) error {
-	if mesString[0] == "PING" {
-		answer := "PONG " + mesString[1] + "\r\n"
-		conn.Write([]byte(answer))
-		fmt.Println("We answered " + answer)
-	}
-	return nil
 }
